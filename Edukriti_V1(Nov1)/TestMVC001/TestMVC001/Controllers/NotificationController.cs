56942434b1cfe41cb4303d1a8340b6a063fd530a
shaileshyerram <shaileshyerram@gmail.com>
2015-11-12 15:51:47 -0700

    modified notification

--- Edukriti_V1(Nov1)/TestMVC001/TestMVC001/Controllers/NotificationController.cs
+++ Edukriti_V1(Nov1)/TestMVC001/TestMVC001/Controllers/NotificationController.cs
@@ -1,16 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Data.SqlClient;
+using System.Net;
+using System.Text;
+using System.Web.Configuration;
 using System.Web.Mvc;
-using TestMVC001.Core.Models;
-using TestMVC001.Core.Services;
 using TestMVC001.Models;
 
 namespace TestMVC001.Controllers
 {
     public class NotificationController : Controller
     {
+        static readonly string ConnectionString = WebConfigurationManager.ConnectionStrings["myConnectionString"].ToString();
         // GET: Notification
         public ActionResult Index()
         {
@@ -18,43 +18,43 @@ namespace TestMVC001.Controllers
         }
 
         [HttpPost]
-        [Authorize(Roles ="Admin")]
+        [Authorize(Roles = "Admin")]
         public ActionResult ProcessNotification(NotificationModel model)
         {
-            static readonly string ConnectionString = WebConfigurationManager.ConnectionStrings["myConnectionString"].ToString();
-            String NotificationSender = "School Admin";
+
+            string NotificationSender = "School Admin";
             String NotificationCategory = "Test";
+
             using (var con = new SqlConnection(ConnectionString))
             {
                 //Insert student attendance record and get the student details to send the SMS
                 con.Open();
 
-                string query = "INSERT INTO[dbo].[tblNotification] ([PhoneNumber],[Sender],[Message],[Category]) VALUES('" + model.ToPhoneNumber + "', '" + NotificationSender + "',  '" + model.NotificationText + "',   '" + NotificationCategory + "') ";
+                string query =
+                    "INSERT INTO[dbo].[tblNotification] ([PhoneNumber],[Sender],[Message],[Category]) VALUES('" +
+                    model.ToPhoneNumber + "', '" + NotificationSender + "',  '" + model.NotificationText + "',   '" +
+                    NotificationCategory + "') ";
                 SqlCommand cmd = new SqlCommand(query, con);
                 cmd.ExecuteNonQuery();
                 con.Close();
 
-                var attendanceResponseModel = new AttendanceResponseModel
-                {
-                    PhoneNumber = cmd1.Parameters["@PhoneNumber"].Value.ToString(),
-                    StudentName = cmd1.Parameters["@studentName"].Value.ToString(),
-                    IsInTime = Convert.ToBoolean(cmd1.Parameters["@isInTime"].Value)
-                };
-                return attendanceResponseModel;
+
+                string bulkUrl =
+                    "http://tra.bulksmshyderabad.co.in/websms/sendsms.aspx?userid=demosms&password=123456&sender=INTECH&";
+                string mobileNo = "9030644017";
+
+                StringBuilder sb = new StringBuilder();
+                sb.Append(bulkUrl);
+                sb.Append("mobileno=" + mobileNo);
+                sb.Append("&msg=" + model.NotificationText);
+
+                var client = new WebClient();
+                var content = client.DownloadString(sb.ToString());
+
+
+
             }
-            // get parameters
-            // store in db
-            //INSERT INTO[dbo].[tblNotification] ([PhoneNumber],[Sender],[Message],[Category]) VALUES(9030644017,'Admin','Test Notification Message','Admin')
-
-
-
-
-            // call smssendservice
-
-            return Content ("Sucessfully Sent Notification");
-
-
+            return View();
         }
-
     }
 }
