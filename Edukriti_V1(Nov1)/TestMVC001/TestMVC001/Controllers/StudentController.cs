f3e6076294f733bad5d6f38dc42da885e6dd531e
shaileshyerram <shaileshyerram@gmail.com>
2015-11-14 21:15:05 -0700

    Add student registration

---
+++ Edukriti_V1(Nov1)/TestMVC001/TestMVC001/Controllers/StudentController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestMVC001.Models;
+
+namespace TestMVC001.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Student/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
+
+        //GET: Student/Details/
+        public ActionResult Details()
+        {
+            var student = new Student
+            {
+                StudentId =143,
+                StudentFirstName = "Shailesh",
+                StudentMiddleName = "Chandra",
+                StudentLastName="Yerram",
+                Class="X",
+                Section = "A",
+                Gender = "M",
+                DateOfBirth = DateTime.Today,
+                Rfid = "0000012345",
+                ParentFirstName = "Santhosh",
+                ParentMiddleName = "Kumar",
+                ParentLastName = "Ragam",
+                ParentPrimaryPhoneNumber = "9030644017",
+                ParentHomePhoneNumber = "9030644017",
+                ParentEmailId = "shaileshyerram@gmail.com",
+                Orgid = "12345"
+
+            };
+            return View(student);
+        }
+
+
+        //bigint StudentId
+        //varchar StudentFirstName
+        //varchar StudentMiddleName
+        //varchar StudentLastName
+        //varchar Class
+        //varchar Section
+        //varchar Gender
+        //datetime DateOfBirth
+        //varchar Rfid
+        //varchar ParentFirstName
+        //varchar ParentMiddleName
+        //varchar ParentLastName
+        //float ParentPrimaryPhoneNumber
+        //float ParentHomePhoneNumber
+        //varchar ParentEmailId
+        //varchar Orgid
+
+
+        // GET: Student/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Student/Create
+        [HttpPost]
+        public ActionResult  Create(Student student ) //Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                if (!ModelState.IsValid)
+                {
+                    return View(student);
+                }
+
+                //return RedirectToAction("Index");
+                return View();
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Student/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Student/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Edukriti_V1(Nov1)/TestMVC001/TestMVC001/Models/Student.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TestMVC001.Models
+{
+    public class Student
+    {
+
+        public int StudentId { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(100,MinimumLength =2 )]
+        public string StudentFirstName { get; set; }
+
+        [Display(Name = "Middle Name")]
+        public string StudentMiddleName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(100, MinimumLength = 2)]
+        public string StudentLastName { get; set; }
+
+        public string Class { get; set; }
+        public string Section { get; set; }
+
+        [Required]
+        [StringLength(1, MinimumLength = 1)]
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        [Display(Name = "RFID #" ) ]        
+        [RegularExpression(@"\d{6,10}")]
+        public string Rfid { get; set; }
+
+        [Required]
+        [Display(Name = "Parent First Name")]
+        [StringLength(100, MinimumLength = 2)]
+        public string ParentFirstName { get; set; }
+
+        [Display(Name = "Parent Middle Name")]
+        [StringLength(100, MinimumLength = 2)]
+        public string ParentMiddleName { get; set; }
+
+        [Required]
+        [Display(Name = "Parent Last Name")]
+        [StringLength(100, MinimumLength = 2)]
+        public string ParentLastName { get; set; }
+
+        [Required]
+        [Display(Name = "Parent Phone Number")]
+        [RegularExpression(@"\d")]
+        public string ParentPrimaryPhoneNumber { get; set; }
+
+        [Display(Name = "Home Phone Number")]
+        [RegularExpression(@"\d")]
+        public string ParentHomePhoneNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Parent Email Address")]
+        [DataType(DataType.EmailAddress)]
+        public string ParentEmailId { get; set; }
+
+
+        public string Orgid { get; set; }
+
+        //public string StudentName
+        //{
+        //    get
+        //    {
+        //        return string.Format("{0} {1} {2}", StudentFirstName, StudentMiddleName, StudentLastName);
+
+        //    }
+        //}
+        public string StudentName => $"{StudentFirstName} {StudentMiddleName} {StudentLastName}";
+        public string ParentName => $"{ParentFirstName} {ParentMiddleName} {ParentLastName}";
+     }
+
+
+}
+
+//bigint StudentId
+//varchar StudentFirstName
+//varchar StudentMiddleName
+//varchar StudentLastName
+//varchar Class
+//varchar Section
+//varchar Gender
+//datetime DateOfBirth
+//varchar Rfid
+//varchar ParentFirstName
+//varchar ParentMiddleName
+//varchar ParentLastName
+//float ParentPrimaryPhoneNumber
+//float ParentHomePhoneNumber
+//varchar ParentEmailId
+//varchar Orgid
